e461e30f2776ec1babcf02468fa2e27876949673
trust9900 <trust9900@yahoo.com.tw>
2016-01-15 10:00:46 +0800

    Java基本型別

--- TestPrimitiveType.java
+++ TestPrimitiveType.java
@@ -1,3 +1,6 @@
+
+import javax.xml.bind.annotation.XmlAccessType;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,7 @@
  */
 public class TestPrimitiveType {
     public static void main(String[] args) {
+        //整數類
      byte a = 10;//10進位
      short b =010;//8近位
      int c =0b010;//2進位 0b開頭2近位
@@ -21,7 +25,7 @@ public class TestPrimitiveType {
         int big =1__1_1;//底線純粹增加辨識度
         System.out.println(big);
        long verybig = 1234567890987654321L;//long型別+L
-       
+       //浮點數類
        float f1 =1.0f-0.8f;
        double d1 = 1.0-0.8;
        
@@ -29,5 +33,6 @@ public class TestPrimitiveType {
         System.out.println("d1 = " + d1);//IEEE754 
         System.out.println(8E-1);//0.8
         System.out.println(1d);//1.0
+        
     }    
 }
