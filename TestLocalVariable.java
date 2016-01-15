e50af40c20e5f675bd636a79210230e56838a59d
trust9900 <trust9900@yahoo.com.tw>
2016-01-15 09:30:53 +0800

    Netbeans U220
    HelloJava

---
+++ TestLocalVariable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestLocalVariable {
+    public static void main(String[] args) {
+        int i;//宣告區域變數i
+        //final int i;//宣告區域常數i
+        int j=100;//宣告變數j為100
+        i=1;//指派i為1
+        System.out.println( "i:"+ i );//1
+        System.out.println("j = " + j);
+    }
+}
---
+++ TestPrimitiveType.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestPrimitiveType {
+    public static void main(String[] args) {
+     byte a = 10;//10進位
+     short b =010;//8近位
+     int c =0b010;//2進位 0b開頭2近位
+     long d =0x010;//16近位
+        System.out.println("a = " + a);//10
+        System.out.println("b = " + b);//8
+        System.out.println("c = " + c);//2
+        System.out.println("d = " + d);//16
+        int big =1__1_1;//底線純粹增加辨識度
+        System.out.println(big);
+       long verybig = 1234567890987654321L;//long型別+L
+       
+       float f1 =1.0f-0.8f;
+       double d1 = 1.0-0.8;
+       
+        System.out.println("f1 = " + f1);//IEEE754 
+        System.out.println("d1 = " + d1);//IEEE754 
+        System.out.println(0b010_1001);
+    }    
+}
